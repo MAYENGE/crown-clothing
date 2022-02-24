36cfc6172d6556c62c9be38e777328c321c49ef8
Simba May <89415362+MAYENGE@users.noreply.github.com>
2022-02-24 08:02:56 +0100

    added sign in with google

--- src/App.js
+++ src/App.js
@@ -4,23 +4,46 @@ import ShopPage from "./pages/shop/shop.component";
 
 import Header from './components/header/header.component.jsx';
 
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
+import { auth } from "./firebase/fire.utils";
+
 import HomePage from "./pages/homepage/homepage.component";
+import React from "react";
 // const HatsPage = () => (
 //   <div>
 //     <h1>Hats Page</h1>
 //   </div>
 // );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      currentUser:null
+    }
+  }
+  unsubscribeFromAuth=null;
+  componentDidMount(){
+   this.unsubscribeFromAuth= auth.onAuthStateChanged( user=>{
+    this.setState({ currentUser: user });
 
-function App() {
+      console.log(user);
+    });
+  }
+  componentWillUnmount(){
+    this.unsubscribeFromAuth()
+  }
+  render(){
   return (
     <div>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/sign" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
 }
-
+}
 export default App;
